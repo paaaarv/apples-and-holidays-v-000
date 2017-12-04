930525bcdaa572cc3c14a94b2a5faaf968f43f79
Parv Kumar <parvathikumar92@gmail.com>
2017-12-04 20:19:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -100,8 +100,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.values.each do|hol|
       if hol.include?("BBQ")
-        new_array << holiday
-        binding.pry
+        new_array << holiday.keys
       end
     end
   end
