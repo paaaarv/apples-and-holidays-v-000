a5ffa698341e440034832fabb4cedf29f22e228b
Parv Kumar <parvathikumar92@gmail.com>
2017-12-04 20:02:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
       puts "#{season.to_s.capitalize}:"
       holiday.keys.each do |hol|
-        new_hol = hol.to_s 
+        new_hol = hol.to_s
         if new_hol.include?("_")
           new_hol.gsub("_", " ")
         end 
